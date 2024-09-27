2bbd7941532c0863c7511e0c791739a7a7a2cf47
Kevin Cullen <157893369+kpcullen@users.noreply.github.com>
2024-09-27 14:51:20 +0200

    update logger

--- utils/middleware.js
+++ utils/middleware.js
@@ -6,7 +6,7 @@ const User = require('../models/user')
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
-  logger.info('Body:  ', request.body)
+  // logger.info('Body:  ', request.body)
   logger.info('---')
   next()
 }
