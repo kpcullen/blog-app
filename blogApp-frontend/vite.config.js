a64dcd9fd8ba6613f759313ab96a30e381af017a
Kevin Cullen <157893369+kpcullen@users.noreply.github.com>
2024-09-27 14:38:02 +0200

    new build

--- blogApp-frontend/vite.config.js
+++ blogApp-frontend/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: 'BlogApp by Kevin Patrick Cullen',
+        },
+      },
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
